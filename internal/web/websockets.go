d8863c4e96472f7bbab5b265f478eade4cd9ef46
Dilitha <diliw101@gmail.com>
2024-12-17 13:23:02 +1300

    Fix err check in websocket msg read

--- internal/web/websockets.go
+++ internal/web/websockets.go
@@ -56,7 +56,7 @@ func (s *Server) readConn(ctx context.Context, sessionId uint64, conn *websocket
 			return
 		}
 		var v userChange
-		if err := json.Unmarshal(bytes, &v); err != nil {
+		if err := json.Unmarshal(bytes, &v); err == nil {
 			s.logger.Info("User change", "sessionId", sessionId, "userId", v.UserId)
 			s.sendUserChange(ctx, sessionId, v.UserId)
 		}
